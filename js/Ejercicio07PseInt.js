52c41dafb138e7589f87596031715a3dc2e566cf
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-10 10:37:22 -0300

    agregue enunciado y complete ejercicio

--- js/Ejercicio07PseInt.js
+++ js/Ejercicio07PseInt.js
@@ -4,3 +4,9 @@ el largo y el ancho de un rectángulo, y calcular su perímetro y área. Las fó
 Perímetro: P = 2*(largo+ancho)
 Área: A=largo×ancho */
 
+const largo = parseInt(prompt("Ingrese largo")),
+  ancho = parseInt(prompt("Ingrese ancho"));
+let perimetro, area;
+perimetro = 2 * (largo + ancho);
+area = largo * ancho;
+document.writeln("Perimetro: " + perimetro + " Area: " + area);
