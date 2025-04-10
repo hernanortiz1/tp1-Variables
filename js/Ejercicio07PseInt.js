dba836b1dc8f528aa5bec6a3f1f740678172258b
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-10 10:33:00 -0300

    agregue link de js a index

---
+++ js/Ejercicio07PseInt.js
@@ -0,0 +1,6 @@
+/*7-Calcular el perímetro y área de un rectángulo: Crear un algoritmo que permita al usuario ingresar 
+el largo y el ancho de un rectángulo, y calcular su perímetro y área. Las fórmulas son:
+
+Perímetro: P = 2*(largo+ancho)
+Área: A=largo×ancho */
+
