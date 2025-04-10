be2d005b6ea75d041bfbfc26fe9ac6c24833d1c0
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-04-10 08:29:21 -0300

    agregue js de ejercicio a index

--- js/Ejercicio01PseInt.js
+++ js/Ejercicio01PseInt.js
@@ -2,3 +2,9 @@
 realizar la suma de ambos y mostrar el resultado.
  */
 
+let edad = parseInt(prompt("Ingresa tu edad"));
+if(edad >= 18){
+    document.writeln("Mayor de edad");
+} else{
+    document.writeln("No es mayor de edad");
+}
