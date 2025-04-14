eeb5ef0360a6bd51335b7ac8f16f8feb3d383f0a
Hernan Ortiz <139821738+hernanortiz1@users.noreply.github.com>
2025-04-14 19:38:06 -0300

    Revert "Readme"

--- js/ejercicio04Tp.js
+++
@@ -1,7 +0,0 @@
-/*4.- Escribe un programa de dos líneas que pida el nombre del usuario con un
-prompt y escriba un texto que diga «Hola nombreUsuario»
-Ejemplo:
-input: Coder Output: Hola Coder
-*/
-const nombre = prompt("Ingrese nombre");
-document.writeln("Hola ", nombre);
